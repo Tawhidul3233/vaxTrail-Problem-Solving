3538b56841973c26d82e1db06362c3ec18580f09
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2023-03-21 09:28:53 +0600

    vaxTrail funcation, 4 variables and conditions decleare

---
+++ vaxTrail.js
@@ -0,0 +1,25 @@
+const  vaxTrail = (array) => {
+
+  // First i decleare 4 variables. Let is used for reassing after decleare
+  let A = [];
+  let B = [];
+  let C = [];
+  let D = [];
+
+  // now  check conditions. This funcation Expects an array 
+  array.forEach(person => {
+    if (person.temperature < 100) {
+     
+    } else {
+      D.push(person);
+    }
+  });
+
+ 
+
+}
+
+
+
+
+
