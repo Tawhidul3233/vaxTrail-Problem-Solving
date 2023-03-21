26339cbd46c72597a71df5bd9f44c872b9c06549
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2023-03-21 09:34:55 +0600

    conditions set done

--- vaxTrail.js
+++ vaxTrail.js
@@ -1,4 +1,4 @@
-const  vaxTrail = (array) => {
+const vaxTrail = (array) => {
 
   // First i decleare 4 variables. Let is used for reassing after decleare
   let A = [];
@@ -9,13 +9,19 @@ const  vaxTrail = (array) => {
   // now  check conditions. This funcation Expects an array 
   array.forEach(person => {
     if (person.temperature < 100) {
-     
+      if (person.age >= 20 && person.age <= 30) {
+        A.push(person);
+      } else if (person.age >= 31 && person.age <= 40) {
+        B.push(person);
+      } else if (person.age >= 41 && person.age <= 50) {
+        C.push(person);
+      }
     } else {
       D.push(person);
     }
   });
 
- 
+
 
 }
 
